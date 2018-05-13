a06362f7cd252208d16a8560d328f31c95e8df9b
otabek1998 <38819245+otabek1998@users.noreply.github.com>
2018-05-13 14:28:42 +0500

    Create about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+
+@section('title', '| About')
+
+@section('content')
+        <div class="row">
+            <div class="col-md-12">
+                <h1>About Us</h1>
+                <p>This is the free blog! Feel free to write anything in comment:)</p>
+            </div>
+        </div>
+
+@endsection

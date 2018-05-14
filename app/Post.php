45d5be24500536c2a7b653cfd9e6261428843d88
otabek1998 <38819245+otabek1998@users.noreply.github.com>
2018-05-14 18:12:57 +0500

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    public function category()
-    {
-    	return $this->belongsTo('App\Category');
-    }
-
+  
     public function tags()
     {
-    	return $this->belongsToMany('App\Tag');
+    	return $this->belongsToMany('App.Tag');
+    }
+     public function comments()
+    {
+    	return $this->hasMany('App.Comment');
+    }
+     public function category()
+    {
+    	return $this->belongsTo('App.Category');
     }
 
-    public function comments()
-    {
-    	return $this->hasMany('App\Comment');
-    }
+   
 }
